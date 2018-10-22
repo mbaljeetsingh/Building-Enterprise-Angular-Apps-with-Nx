34d540b07d30d9e4cdc5403e609e075353f28be9
Baljeet Singh <bsinghs.007@gmail.com>
2018-10-22 18:06:48 +0530

    add signin containers, compoents (client lib)

--- libs/client/auth/src/lib/client-auth.module.ts
+++ libs/client/auth/src/lib/client-auth.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SigninComponent } from './containers/signin/signin.component';
+import { SigninFormComponent } from './components/signin-form/signin-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
 
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      { path: '', pathMatch: 'full', component: SigninComponent }
     ])
-  ]
+  ],
+  declarations: [SigninComponent, SigninFormComponent]
 })
 export class ClientAuthModule {}
---
+++ libs/client/auth/src/lib/components/signin-form/signin-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nxt-signin-form',
+  templateUrl: './signin-form.component.html',
+  styleUrls: ['./signin-form.component.scss']
+})
+export class SigninFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
